cd8e1ea4cfa1838563664b2253920b99be0fc652
Alexx <alexbantjes376@gmail.com>
2025-08-20 13:23:20 +0200

    feat: auto refresh dashboard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { useEffect } from 'react';
 import AppRoutes from './routes/AppRoutes';
 
 function App() {
+  // Refresh the page every 30 seconds so graphs update without manual input
+  useEffect(() => {
+    const id = setInterval(() => {
+      // Reload from cache to minimize any visible flicker
+      window.location.reload(false);
+    }, 30000);
+
+    return () => clearInterval(id);
+  }, []);
+
   return <AppRoutes />;
 }
 
